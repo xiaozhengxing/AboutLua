368543e85b2914f02e1daed94791146248c1cd00
xiaozhengxing <1084287435@qq.com>
2022-12-10 23:33:05 +0800

    学习云风 snapShot

---
+++ projs/AboutLua/TestLua/SnapShot.cpp
@@ -0,0 +1,64 @@
+/*
+参考
+https://github.com/cloudwu/lua-snapshot
+*/
+
+#include <iostream>
+#include <cassert>
+#include <windows.h>
+#include <map>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+extern "C"
+{
+#include "../Lua5_3_5/lua.h"
+#include "../Lua5_3_5/lauxlib.h"
+#include "../Lua5_3_5/lualib.h"
+#include "../Lua5_3_5/ltable.h"
+#include "../Lua5_3_5/lstate.h"
+#include "../Lua5_3_5/lobject.h"
+#include "../Lua5_3_5/lapi.h"
+#include "../Lua5_3_5/lgc.h"
+
+}
+
+
+void mainSnapShot()
+{
+	lua_State* L = luaL_newstate();
+	assert(L != NULL);
+
+	luaopen_base(L);
+	luaL_openlibs(L);
+
+	int iRet = luaL_dofile(L, "SnapShot.lua");
+	if (iRet)
+	{
+		cout << "load file SnapShot.lua error " << iRet << endl;
+		const char* pErrorMsg = lua_tostring(L, -1);
+		cout << pErrorMsg << endl;
+		lua_close(L);
+
+		return;
+	}
+
+	iRet = luaL_dofile(L, "DoSnapShot.lua");
+	if (iRet)
+	{
+		cout << "load file DoSnapShot.lua error " << iRet << endl;
+		const char* pErrorMsg = lua_tostring(L, -1);
+		cout << pErrorMsg << endl;
+		lua_close(L);
+
+		return;
+	}
+}
+
+
+void main()
+{
+	mainSnapShot();
+}
